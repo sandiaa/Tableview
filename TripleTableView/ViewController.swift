60568eae79e1f67eaab475266557d6bcdf25e56f
Admin <admin@admins-macbook-pro-2.local>
2018-12-19 13:23:26 +0530

    Initial Commit

---
+++ TripleTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TripleTableView
+//
+//  Created by Admin on 19/12/18.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
